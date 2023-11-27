dfb9015bde661ef83a2e8ec4e340ff89af5a5ddc
StBinge <964341818@qq.com>
2023-11-27 18:00:58 +0800

    change host

--- server.py
+++ server.py
@@ -83,4 +83,4 @@ async def delete_env(key:str):
 if __name__ == '__main__':
     schedule.run()
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=9191)
+    uvicorn.run(app, host='127.0.0.1', port=9191)
